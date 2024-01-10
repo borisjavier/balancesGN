13e1b485c46c91279b945f542972f424076921c1
migue <miguelfeliped@gmail.com>
2024-01-10 18:01:35 -0300

    balance route working

--- functions/api/routes.js
+++ functions/api/routes.js
@@ -1,45 +1,36 @@
 const express = require('express')
-const logger = require("firebase-functions/logger")
+const { Address } = require('@runonbitcoin/nimble')
+const bodyParser = require('body-parser')
+const { load } = require("../model/run")
 
-const TRUSTED = ['a5c5b72267ea32eab1ff4c7a87da1d2c8515ddb260d88c05eb84b2c16e393e48_o1']
+const GDN_TOKEN_ORIGIN = 'a5c5b72267ea32eab1ff4c7a87da1d2c8515ddb260d88c05eb84b2c16e393e48_o1'
 
 function buildRoutes (run, logger) {
     const app = express()
+    app.use(bodyParser.json())
 
-    async function balanceFor (privateKey) {
-        run.owner = privateKey
-        await run.inventory.sync()
-        const jigs = run.inventory.jigs
-            .filter(j => j.constructor.origin === 'a5c5b72267ea32eab1ff4c7a87da1d2c8515ddb260d88c05eb84b2c16e393e48_o1')
-        return jigs.map(j => j.amount).reduce((a, b) => a + b, 0)
+    async function balanceFor (rawAddress) {
+        const address = Address.fromString(rawAddress)
+        const utxos = await run.blockchain.utxos(address.toScript().toHex())
+        const locations = utxos.map(u => `${u.txid}_o${u.vout}`)
+        const maybeJigs = await Promise.all(locations.map(async loc => load(run, loc)))
+        const jigs = maybeJigs.filter(a => a)
+
+        const tokens = jigs
+            .filter(j => j.constructor.origin === GDN_TOKEN_ORIGIN)
+        let balance = tokens.map(j => j.amount).reduce((a, b) => a + b, 0)
+        return { balance, address: address.toString() }
     }
 
-    app.get('/cuenta1', async (req, res) => {
-        // run.owner.address
-        res.json({ balance: await balanceFor('L5dGUVnAKNf3FYwgzh3Lb8wkPGpqrDDyWUthcwJYUGuLxHMvtFuF'), address: run.owner.address })
+    app.get('/utxos', async (req, res) => {
+        console.log(run.blockchain)
+        res.send('ok')
     })
 
-    app.get('/cuenta2', async (req, res) => {
-        res.json({ balance: await balanceFor('L5UpCrzQfXtjmNKax62qH6yJfczMpcdgCKeK9nHvW25QQoLdB8e2') })
-    })
-
-    app.get('/cuenta3', async (req, res) => {
-        res.json({ balance: await balanceFor('L1XAKaZEqDPafgF3WRaxZM1jDpc7Dd331UjohQ2T5X1WCiTFy9dK') })
-    })
-
-    app.get('/send-desde-cuenta-3', async (req, res) => {
-        run.owner = 'L1XAKaZEqDPafgF3WRaxZM1jDpc7Dd331UjohQ2T5X1WCiTFy9dK'
-        await run.inventory.sync()
-        const jigs = run.inventory.jigs
-            .filter(j => j.constructor.origin === 'a5c5b72267ea32eab1ff4c7a87da1d2c8515ddb260d88c05eb84b2c16e393e48_o1')
-
-        const token = jigs[0]
-        const address = "1HnhPzq6Zu3DLtRsmtVsvtpExdH77ziw6X"
-        const otherToken = token.send(address)
-        await token.sync()
-        await otherToken.sync()
-        res.json({ location: otherToken.location })
-
+    app.post('/balance', async (req, res) => {
+        const body = req.body
+        const ownerAddress = body.ownerAddress
+        res.json(await balanceFor(ownerAddress))
     })
 
     return app
